86ea6cffbdd4f96de5c90a4c7004120ed3bf299e
Mat Swatek <azcards32@gmail.com>
2024-09-28 17:40:11 -0700

    Update ACutAbove.py

--- ACutAbove.py
+++ ACutAbove.py
@@ -242,7 +242,7 @@ conditions = [
     (result['date2'] > '2023-09-11')
     ]
 
-values = ['1','18','17','16','15','14','13','12','11','10','9','8','7','6','5','4','3','2']
+values = [1,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2]
 
 result['week'] = np.select(conditions, values) #as defined above
 
